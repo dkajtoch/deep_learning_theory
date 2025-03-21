adds hessian and fixes

src/deep_learning_theory/datamodules.py
@@ -6,7 +6,7 @@ import numpy as np
 import torchvision.transforms as transform_lib
 from lightning import LightningDataModule
 from torch import Tensor
-from torch.utils.data import DataLoader, Dataset, IterableDataset, Subset
+from torch.utils.data import DataLoader, Dataset, Subset
 from torchvision.datasets import CIFAR10, CIFAR100, MNIST, ImageNet
 
 
@@ -53,16 +53,16 @@ class VisionDataModule(LightningDataModule):
     def setup(self, stage: str | None = None) -> None:
         """Creates train, val, and test dataset."""
         if stage == "fit" or stage is None:
-            dataset_train = self.dataset_cls(
+            self.dataset_train = self.dataset_cls(
                 self.data_dir,
                 train=True,
                 transform=self.train_transform,
             )
             if self.num_samples is not None:
                 indices = self._create_balanced_sample(
-                    dataset_train, self.num_samples, self.num_classes
+                    self.dataset_train, self.num_samples, self.num_classes
                 )
-                self.dataset_train = Subset(dataset_train, indices)
+                self.dataset_train = Subset(self.dataset_train, indices)
 
             self.dataset_val = self.dataset_cls(
                 self.data_dir,
@@ -92,7 +92,7 @@ class VisionDataModule(LightningDataModule):
 
     @staticmethod
     def _create_balanced_sample(
-        dataset: IterableDataset, num_samples: int, num_classes: int
+        dataset: Dataset, num_samples: int, num_classes: int
     ) -> list[int]:
         rng = np.random.RandomState(42)
 
@@ -106,7 +106,8 @@ class VisionDataModule(LightningDataModule):
 
         class_indices: list[list[int]] = [[] for _ in range(num_classes)]
 
-        for idx, (_, label) in enumerate(dataset):
+        for idx in range(len(dataset)):  # type: ignore[arg-type]
+            _, label = dataset[idx]
             if isinstance(label, Tensor):
                 label = label.item()
             class_indices[label].append(idx)

src/deep_learning_theory/main.py
@@ -1,13 +1,20 @@
+from typing import Any, Iterator
+
 import hydra
 import torch
 from lightning import LightningDataModule, LightningModule, Trainer, seed_everything
-from lightning.pytorch.callbacks import LearningRateMonitor
-from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger, CSVLogger
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
+import os
+import numpy as np
+from pyhessian import hessian
+from torch.utils.data import DataLoader
 
 from deep_learning_theory.callbacks import GradientNormMonitor
 from deep_learning_theory.utils import flatten_dict
+from tqdm.auto import tqdm
 
 # Models:
 # - Linear network
@@ -22,6 +29,41 @@ def store_config_in_loggers(trainer: Trainer, cfg: DictConfig) -> None:
             _logger.experiment.config.update(flat_dict)
         else:
             _logger.log_hyperparams({"config": flat_dict})
+
+
+def update_model_weights_from_ckpt(
+    checkpoint_path: str, module: LightningModule
+) -> tuple[int, int]:
+    ckpt = torch.load(checkpoint_path)
+    module.load_state_dict(ckpt["state_dict"])
+    return ckpt["epoch"], ckpt["global_step"]
+
+
+def collect_hessian_stats(
+    dir_path: str, module: LightningModule, dataloader: list[Any]
+) -> Iterator[tuple[Any, ...]]:
+    max_iter = 1000
+    tol = 1e-5
+
+    files = sorted(os.listdir(dir_path))
+    files = list(filter(lambda x: x.endswith("ckpt"), files))
+    for filename in tqdm(files, desc="Gathering Hessian stats"):
+        path = os.path.join(dir_path, filename)
+        ckpt = torch.load(path)
+        module.load_state_dict(ckpt["state_dict"])
+        global_step = ckpt["global_step"]
+
+        hessian_comp = hessian(
+            model=module.backbone,
+            criterion=module.loss,
+            dataloader=dataloader,
+            device=module.device
+        )
+        top_eigenvalues, _ = hessian_comp.eigenvalues(
+            top_n=1, maxIter=max_iter, tol=tol
+        )
+        trace = np.mean(hessian_comp.trace(maxIter=max_iter, tol=tol))
+        yield {"top_eigenvalue": top_eigenvalues[0]}, {"trace": trace}, global_step
 
 
 @hydra.main(config_path="conf", config_name="base", version_base="1.2")
@@ -44,10 +86,30 @@ def main(cfg: DictConfig) -> None:
                 log_weight_decay=True,
             ),
             GradientNormMonitor(),
+            ModelCheckpoint(
+                save_weights_only=True,
+                save_on_train_epoch_end=True,
+                save_top_k=-1,
+                every_n_train_steps=cfg.ckpt_every_n_train_steps,
+            ),
         ],
+        enable_checkpointing=True,
     )
     store_config_in_loggers(trainer, cfg)
     trainer.fit(model=module, datamodule=datamodule)
+
+    ckpt_dir_path = trainer.checkpoint_callback.dirpath
+
+    data = []
+    for batch in datamodule.train_dataloader():
+        data.append(batch)
+
+    for top_eigen, trace, global_step in collect_hessian_stats(
+        ckpt_dir_path, module, data
+    ):
+        for _logger in trainer.loggers:
+            _logger.log_metrics(top_eigen, step=global_step)
+            _logger.log_metrics(trace, step=global_step)
 
 
 if __name__ == "__main__":